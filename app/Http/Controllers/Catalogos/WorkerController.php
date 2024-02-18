ef9c0038730514a49fcdc8288252908757cc8946
efrain-dev <efraindeleon12@outlook.com>
2024-02-17 20:07:52 -0600

    Correcion

--- app/Http/Controllers/Catalogos/WorkerController.php
+++ app/Http/Controllers/Catalogos/WorkerController.php
@@ -14,7 +14,7 @@ class WorkerController extends Controller
     public function index(Request $request)
     {
         [$hire, $birthdate, $filter, $active] = $this->getDates($request);
-        $workers = DB::table('worker as w')
+        $query = DB::table('worker as w')
             ->where(function ($query) use ($filter) {
                 $query = $query->orWhere('w.name', 'like', '%' . $filter . '%');
                 $query = $query->orWhere('w.last_name', 'like', '%' . $filter . '%');
@@ -23,26 +23,33 @@ class WorkerController extends Controller
                 $query = $query->orWhere('w.email', 'like', '%' . $filter . '%');
                 $query = $query->orWhere('w.address', 'like', '%' . $filter . '%');
                 $query = $query->orWhere('w.contact', 'like', '%' . $filter . '%');
+                $query = $query->orWhere('w.cel', 'like', '%' . $filter . '%');
+
             });
+
         if ($hire['active']) {
-            $workers = $workers->whereBetween('w.date_in', [$hire['from'], $hire['to']]);
+
+            $query = $query->whereBetween('w.date_in', [$hire['from'], $hire['to']]);
         }
         if ($birthdate['active']) {
-            $workers = $workers->whereBetween('w.birthdate', [$birthdate['from'], $birthdate['to']]);
+            $query = $query->whereBetween('w.birthdate', [$birthdate['from'], $birthdate['to']]);
         }
         switch ($active) {
-            case 1:
+            case '1':
             {
-                $workers = $workers->where('w.active', '=', 1);
+                $query = $query->where('w.active', '=', 1);
                 break;
             }
-            case 0:
+            case '0':
             {
-                $workers = $workers->where('w.active', '=', 0);
+                $query = $query->where('w.active', '=', 0);
                 break;
             }
+            default:
+
         }
-        $workers = $workers->get();
+
+        $workers = $query->get();
         return response()->json($workers);
     }
 
@@ -52,14 +59,15 @@ class WorkerController extends Controller
         $birthdate = $request->get('birthdate');
         $filter = $request->get('filter') ?: '';
         $active = $request->get('active');
-        if ($hire['active']){
-            $hire['from'] = $hire['from'] ?   Carbon::createFromFormat('Y-m-d', $hire['from']) : Carbon::now()->startOfYear()->startOfMonth();
-            $hire['to'] = $hire['to'] ?  Carbon::createFromFormat('Y-m-d', $hire['to']) ->addDay() : Carbon::now()->addMonth()->startOfMonth();
+        if ($hire['active']) {
+            $hire['from'] = $hire['from'] ? Carbon::createFromFormat('Y-m-d', $hire['from']) : Carbon::now()->startOfYear()->startOfMonth();
+            $hire['to'] = $hire['to'] ? Carbon::createFromFormat('Y-m-d', $hire['to'])->addDay() : Carbon::now()->addMonth()->startOfMonth();
         }
-        if ($birthdate['active']){
-            $birthdate['from'] = $birthdate['from'] ? Carbon::createFromFormat('Y-m-d', $birthdate['from'])   : Carbon::now()->startOfMonth();
-            $birthdate['to'] = $birthdate['to'] ? Carbon::createFromFormat('Y-m-d', $birthdate['to']) ->addDay() : Carbon::now()->addMonth()->startOfMonth();
+        if ($birthdate['active']) {
+            $birthdate['from'] = $birthdate['from'] ? Carbon::createFromFormat('Y-m-d', $birthdate['from']) : Carbon::now()->startOfMonth();
+            $birthdate['to'] = $birthdate['to'] ? Carbon::createFromFormat('Y-m-d', $birthdate['to'])->addDay() : Carbon::now()->addMonth()->startOfMonth();
         }
+
         return [$hire, $birthdate, $filter, $active];
     }
 
@@ -82,9 +90,9 @@ class WorkerController extends Controller
 
             ]);
             $data = $request->all();
-            $data['date_in'] = $data['date_in']? Carbon::createFromFormat('Y-m-d', $data['date_in']):'';
-            $data['date_out'] = $data['date_out']? Carbon::createFromFormat('Y-m-d', $data['date_out']):'';
-            $data['birthdate'] = $data['birthdate']? Carbon::createFromFormat('Y-m-d', $data['birthdate']):'';
+            $data['date_in'] = $data['date_in'] ? Carbon::createFromFormat('Y-m-d', $data['date_in']) : '';
+            $data['date_out'] = $data['date_out'] ? Carbon::createFromFormat('Y-m-d', $data['date_out']) : '';
+            $data['birthdate'] = $data['birthdate'] ? Carbon::createFromFormat('Y-m-d', $data['birthdate']) : '';
             Worker::create($data);
             return response()->json([
                 'status' => 1,
@@ -160,11 +168,11 @@ class WorkerController extends Controller
             ], 500);
         }
     }
+
     public function sendMail()
     {
         $data['email'] = 'efraindeleon12@outlook.com';
         $data['title'] = 'Report LastPay';
-
 
 
         Mail::to('efraindeleon12@outlook.com');
